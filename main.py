2d4d3399fff52be10689006cf4eb4fdeff2510d0
cgrambow <cgrambow@mit.edu>
2017-11-06 12:10:43 -0500

    Refactor code

--- main.py
+++ main.py
@@ -30,17 +30,18 @@ import os
 import bcolz
 import numpy as np
 
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Activation, LSTM, Bidirectional, TimeDistributed, Masking
-from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.preprocessing.sequence import pad_sequences
 
+from models import blstm
 from util import shuffle_arrays
 
 
 def main():
     np.random.seed(7)
     seq_dir, ss_dir, out_dir = parse_arguments()
+
+    if out_dir is None:
+        out_dir = os.getcwd()
 
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
@@ -49,50 +50,37 @@ def main():
     # `maxseq` only affects how many files to read, it doesn't actually
     # correspond to the number of training points. It's probably a good idea to
     # read all files and then specify a smaller subset for training/testing in
-    # `make_data_tensors`.
+    # `make_data_tensors`. A value of -1 will read all files.
     maxseq = -1
     seqs, sss = read_seqs_and_sss(seq_dir, ss_dir, maxseq=maxseq)
+    np.save(os.path.join(out_dir, 'seqs_dict.npy'), seqs)  # Save dictionaries
+    np.save(os.path.join(out_dir, 'sss_dict.npy'), sss)
 
-    # Construct data for Keras. This pads sequences with rows of zero for ones
+    # Construct data for Keras. This pads sequences with rows of zeros for ones
     # that are shorter than the longest sequence in `seqs`.
-    x, y = make_data_tensors(seqs, sss, ndata=5000)
-    assert x.shape[:2] == y.shape[:2]
+    ndata = 5000  # Specify number of data points (includes train, val, and test)
+    x, y = make_data_tensors(seqs, sss, ndata=ndata)
 
-    train_split = 0.8
+    train_split = 0.8  # Fraction of points to use as training data. Rest is divided equally into val/test
     train_end = int(train_split * len(x))
     val_end = int((1.0 - (1.0 - train_split) / 2.0) * len(x))
     x_train, x_val, x_test = x[:train_end], x[train_end:val_end], x[val_end:]
     y_train, y_val, y_test = y[:train_end], y[train_end:val_end], y[val_end:]
+    np.save(os.path.join(out_dir, 'x_train.npy'), x_train)  # Save data
+    np.save(os.path.join(out_dir, 'x_val.npy'), x_val)
+    np.save(os.path.join(out_dir, 'x_test.npy'), x_test)
+    np.save(os.path.join(out_dir, 'y_train.npy'), y_train)
+    np.save(os.path.join(out_dir, 'y_val.npy'), y_val)
+    np.save(os.path.join(out_dir, 'y_test.npy'), y_test)
     
-    num_samples = x_train.shape[0]
-    max_seq_len = x_train.shape[1]
-    num_features = x_train.shape[2]
-    num_classes = y_train.shape[2]
-
-    # Build Keras model
-    hidden_units = 32
-    max_epochs = 500
+    # Set parameters for Keras model
+    hidden_units = 32  # Tune this
+    max_epochs = 1000
     batch_size = 32
-    
-    model = Sequential()
-    model.add(Masking(mask_value=0, input_shape=(max_seq_len, num_features)))
-    model.add(Bidirectional(LSTM(hidden_units, return_sequences=True, input_shape=(max_seq_len, num_features))))
-    model.add(TimeDistributed(Dense(num_classes)))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
-    
-    # Train model. Use early-stopping on validation data to determine when to stop training.
-    model_path = os.path.join(out_dir, 'blstm_model.h5')
-    checkpointer = ModelCheckpoint(model_path, save_best_only=True)
-    model.fit(x_train, y_train, epochs=max_epochs, batch_size=batch_size, verbose=2,
-              validation_data=(x_val, y_val), callbacks=[EarlyStopping(patience=10), checkpointer])
 
-    model = load_model(model_path)  # Best model is not necessarily current model instance b/c patience != 0
-    score_train = model.evaluate(x_train, y_train)[1]
-    score_test = model.evaluate(x_test, y_test)[1]
-    print('Train accuracy: {:.2f}%'.format(score_train * 100.0))
-    print('Test accuracy: {:.2f}%'.format(score_test * 100.0))
+    # Build model and train
+    model = blstm(x_train, x_val, x_test, y_train, y_val, y_test, out_dir,
+                  hidden_units=hidden_units, max_epochs=max_epochs, batch_size=batch_size)
 
 
 def parse_arguments():
@@ -103,18 +91,18 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('seq_dir', type=str, metavar='SEQ_DIR', help='Directory containing encoded protein sequences.')
     parser.add_argument('ss_dir', type=str, metavar='SS_DIR', help='Directory containing encoded secondary structures')
-    parser.add_argument('-o', '--out_dir', type=str, metavar='OUT_DIR', help='Directory to save output in (such as Keras models')
+    parser.add_argument('-o', '--out_dir', type=str, metavar='OUT_DIR', help='Directory to save output in')
     args = parser.parse_args()
 
     return args.seq_dir, args.ss_dir, args.out_dir
 
 
-def read_seqs_and_sss(seq_dir, ss_dir, maxseq=-1):
+def read_seqs_and_sss(seq_dir, ss_dir, maxseq=-1, max_len=None):
     """
     Read bcolz files containing one-hot representations of protein sequences 
     (in `seq_dir`) and secondary structure annotations (in `ss_dir`). For faster
     execution during debugging, a maximum number of sequences to be read can be
-    specified.
+    specified. The maximum sequence length can be restricted using `max_len`.
 
     Returns dictionaries of sequences and secondary structures (dictionary key
     is the file name without its extension).
@@ -129,9 +117,11 @@ def read_seqs_and_sss(seq_dir, ss_dir, maxseq=-1):
 
         # For now ignore proteins that have unknown amino acids or the end character
         if not np.any(seq[:,20]) and not np.any(seq[:,23]):
-            name = os.path.splitext(seq_f_base)[0]
-            seqs[name] = seq
-            sss[name] = ss
+            # Only add sequences up to specified length
+            if max_len is None or len(seq) <= max_len:
+                name = os.path.splitext(seq_f_base)[0]
+                seqs[name] = seq
+                sss[name] = ss
 
         if len(seqs) == maxseq:
             break
@@ -149,11 +139,12 @@ def make_data_tensors(seqs, sss, ndata='all'):
     """
     x = pad_sequences(seqs.values(), padding='post')
     y = pad_sequences(sss.values(), padding='post')
+    assert x.shape[:2] == y.shape[:2]
     x, y = shuffle_arrays(x, y)
 
     if ndata == 'all':
         return x, y
-    elif isinstance(ndata, (int, long)):
+    elif isinstance(ndata, int):
         return x[:ndata], y[:ndata]
     else:
         raise Exception('ndata has invalid type')
---
+++ models.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation, LSTM, Bidirectional, TimeDistributed, Masking
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+def blstm(x_train, x_val, x_test, y_train, y_val, y_test, out_dir,
+          name='blstm_model', hidden_units=10, max_epochs=1000, batch_size=32, patience=20):
+    """
+    Bidirectional LSTM model for protein secondary structure prediction.
+    """
+    num_samples = x_train.shape[0]
+    max_seq_len = x_train.shape[1]
+    num_features = x_train.shape[2]
+    num_classes = y_train.shape[2]
+    
+    # Build Keras model
+    model = Sequential()
+    model.add(Masking(mask_value=0, input_shape=(max_seq_len, num_features)))
+    model.add(Bidirectional(LSTM(hidden_units, return_sequences=True, input_shape=(max_seq_len, num_features))))
+    model.add(TimeDistributed(Dense(num_classes)))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
+    
+    # Train model. Use early-stopping on validation data to determine when to stop training.
+    model_path = os.path.join(out_dir, name + '.h5')
+    checkpointer = ModelCheckpoint(model_path, save_best_only=True)
+    model.fit(x_train, y_train, epochs=max_epochs, batch_size=batch_size, verbose=2,
+              validation_data=(x_val, y_val), callbacks=[EarlyStopping(patience=patience), checkpointer])
+
+    model = load_model(model_path)  # Best model is not necessarily current model instance b/c patience != 0
+    score_train = model.evaluate(x_train, y_train)[1]
+    score_test = model.evaluate(x_test, y_test)[1]
+    print('Train accuracy: {:.2f}%'.format(score_train * 100.0))
+    print('Test accuracy: {:.2f}%'.format(score_test * 100.0))
+
+    return model
